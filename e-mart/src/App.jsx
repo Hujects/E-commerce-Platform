41305860c37cd44239a1f118ce79830e9abfcf40
Ryhan Rafi <rafirafi190400@gmail.com>
2026-07-26 12:26:02 +0600

    a

---
+++ e-mart/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  import PrimaryButton from "./components/UIKIT/PrimaryButton";
+
+  return (
+    <div className="bg-amber-500 w-full h-screen">
+      {/* <PrimaryButton /> */}
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
