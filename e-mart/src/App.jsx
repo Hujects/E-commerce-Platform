407b352e3a38b126a1d0bea8f16c208483c60133
Habibulla <habibulla9177@gmail.com>
2026-08-04 18:27:45 +0600

    topheader.jsx

--- e-mart/src/App.jsx
+++ e-mart/src/App.jsx
@@ -1,12 +1,14 @@
 import MainProductCard from "./components/UIKIT/MainProductCard";
 import TitleCard from "./components/UIKIT/TitleCard";
 import TitleCetegoryPriceCard from "./components/UIKIT/TitleCetegoryPriceCard";
+import TopHeader from './components/header/TopHeader';
 function App() {
   return (
     <div className="flex justify-center items-center w-full h-screen">
       {/* <TitleCetegoryPriceCard /> */}
       {/* <TitleCard /> */}
-      <MainProductCard />
+      {/* <MainProductCard /> */}
+      <TopHeader />
     </div>
   );
 }
