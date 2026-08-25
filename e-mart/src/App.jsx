8e249f04777df1e891769d61c8de75318cc765c3
Ryhan Rafi <rafirafi190400@gmail.com>
2026-08-25 09:54:39 +0600

    react router installed

--- e-mart/src/App.jsx
+++ e-mart/src/App.jsx
@@ -1,9 +1,15 @@
-import Home from './pages/Home/Home';
+import {Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import Cetegory from "./pages/Cetegory/Cetegory";
 function App() {
   return (
-    <div className="h-screen max-w-[var(--max-width)] ">
-      <Home />
-    </div>
+      <div className="min-h-screen max-w-[var(--max-width)] mx-auto">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cetegory" element={<Cetegory />} />
+        </Routes>
+      </div>
   );
 }
 
---
+++ e-mart/src/pages/Cetegory/Cetegory.jsx
@@ -0,0 +1,6 @@
+
+export default function Cetegory() {
+  return (
+    <div className="h-screen w-full bg-red-600">Cetegory</div>
+  )
+}
