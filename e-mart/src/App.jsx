296afd066fef28ab775004c7d17034450f52e21e
Ryhan Rafi <rafirafi190400@gmail.com>
2026-09-01 07:39:53 +0600

    product overview section added

--- e-mart/src/App.jsx
+++ e-mart/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import Cetegory from "./pages/Cetegory/Cetegory";
+import Product from "./pages/Product/Product";
 function App() {
   return (
     <div className="min-h-screen mx-auto">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cetegory" element={<Cetegory />} />
+        <Route path="/product" element={<Product />} />
       </Routes>
     </div>
   );
